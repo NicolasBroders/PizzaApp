3baa2a4ed0c8a2dacf2af655252d554b7d217c3e
PapailleEnQueue <contact@nicolas-broders.info>
2020-10-17 16:20:33 +0200

    Add sort options and persistence

--- PizzaApp/PizzaApp/MainPage.xaml.cs
+++ PizzaApp/PizzaApp/MainPage.xaml.cs
@@ -14,22 +14,35 @@ namespace PizzaApp
 {
     public partial class MainPage : ContentPage
     {
+        public enum e_tri { TRI_AUCUN,
+                            TRI_NOM,
+                            TRI_PRIX };
 
         List<Pizza> pizzas;
+        e_tri tri;
+
+        const string KEY_TRI = "tri";
         public MainPage()
         {
             InitializeComponent();
-            
 
+
+            tri = e_tri.TRI_AUCUN;
             listView.IsVisible = false;
             waitLayout.IsVisible = true;
+
+            if (Application.Current.Properties.ContainsKey(KEY_TRI))
+            {
+                tri = (e_tri)Application.Current.Properties[KEY_TRI];
+                sortImage.Source = GetImageSourceFromTri(tri);
+            }
 
             listView.RefreshCommand = new Command((obj) =>
             {
                 DownloadData((pizzas) =>
                 {
                     listView.IsRefreshing = false;
-                    listView.ItemsSource = pizzas;
+                    listView.ItemsSource = GetPizzasFromTri(tri, pizzas);
                 });
 
             });
@@ -39,7 +52,7 @@ namespace PizzaApp
             {
                 listView.IsVisible = true;
                 waitLayout.IsVisible = false;
-                listView.ItemsSource = pizzas;
+                listView.ItemsSource = GetPizzasFromTri(tri, pizzas);
             });
 
 
@@ -77,6 +90,84 @@ namespace PizzaApp
 
                 webClient.DownloadStringAsync(new Uri(URL));
             }
+        }
+
+        private void SortButtonClicked(object sender, EventArgs e)
+        {
+            switch (tri)
+            {
+                case e_tri.TRI_AUCUN:
+                    tri = e_tri.TRI_NOM;
+                    break;
+
+                case e_tri.TRI_NOM:
+                    tri = e_tri.TRI_PRIX;
+                    break;
+
+                case e_tri.TRI_PRIX:
+                    tri = e_tri.TRI_AUCUN;
+                    break;
+            }
+            sortImage.Source = GetImageSourceFromTri(tri);
+            listView.ItemsSource = GetPizzasFromTri(tri, pizzas);
+
+            Application.Current.Properties[KEY_TRI] = (int)tri;
+            Application.Current.SavePropertiesAsync();
+
+        }
+
+        public String GetImageSourceFromTri(e_tri t)
+        {
+            string res = "";
+            switch (tri)
+            {
+                case e_tri.TRI_AUCUN:
+                    res = "sort_none";
+                    break;
+
+                case e_tri.TRI_NOM:
+                    res = "sort_nom";
+                    break;
+
+                case e_tri.TRI_PRIX:
+                    res = "sort_prix";
+                    break;
+
+                default:
+                    res = "sort_none";
+                    break;
+            }
+            return res;
+        }
+
+        public List<Pizza> GetPizzasFromTri(e_tri t, List<Pizza> l)
+        {
+            if (l == null)
+            {
+                return null;
+            }
+
+            List<Pizza> copy = new List<Pizza>(l);
+
+            switch (tri)
+            {
+                case e_tri.TRI_AUCUN:
+                    return l;
+                    break;
+
+                case e_tri.TRI_NOM:
+                    copy.Sort((emp1, emp2) => emp1.Titre.CompareTo(emp2.Titre));
+                    break;
+
+                case e_tri.TRI_PRIX:
+                    copy.Sort((emp1, emp2) => emp2.prix.CompareTo(emp1.prix));
+                    break;
+
+                default:
+                    return l;
+                    break;
+            }
+            return copy;
         }
     }
 }
