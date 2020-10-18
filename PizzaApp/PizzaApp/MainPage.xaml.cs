f0724e4012172e427efb4c19e86c4eabff51e5cd
PapailleEnQueue <contact@nicolas-broders.info>
2020-10-18 13:10:30 +0200

    Add favorite option

--- PizzaApp/PizzaApp/MainPage.xaml.cs
+++ PizzaApp/PizzaApp/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using PizzaApp.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -16,20 +17,50 @@ namespace PizzaApp
     {
         public enum e_tri { TRI_AUCUN,
                             TRI_NOM,
-                            TRI_PRIX };
+                            TRI_PRIX,
+                            TRI_FAV};
 
         List<Pizza> pizzas;
+        List<String> pizzasFav = new List<String>();
+        
         e_tri tri;
 
+        string jsonFile;
+        string tempFile;
+        string favFile;
+
         const string KEY_TRI = "tri";
+        const string KEY_FAV = "fav";
         public MainPage()
         {
             InitializeComponent();
 
+            /*
+            pizzasFav.Add("4 fromages");
+            pizzasFav.Add("indienne");
+            pizzasFav.Add("tartiflette");
+            */
+
+            tempFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "temp.json");
+            jsonFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "pizzas.json");
+
+            favFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "fav.json");
 
             tri = e_tri.TRI_AUCUN;
             listView.IsVisible = false;
             waitLayout.IsVisible = true;
+
+            if(File.Exists(jsonFile))
+            {
+                string pizzasJson = File.ReadAllText(jsonFile);
+                if (!String.IsNullOrEmpty(pizzasJson))
+                {
+                    pizzas = JsonConvert.DeserializeObject<List<Pizza>>(pizzasJson);
+                    listView.ItemsSource = GetPizzaCells(GetPizzasFromTri(tri, pizzas),pizzasFav);
+                    listView.IsVisible = true;
+                    waitLayout.IsVisible = false;
+                }
+            }
 
             if (Application.Current.Properties.ContainsKey(KEY_TRI))
             {
@@ -37,22 +68,35 @@ namespace PizzaApp
                 sortImage.Source = GetImageSourceFromTri(tri);
             }
 
+
+            LoadFavList();
+
+
+
             listView.RefreshCommand = new Command((obj) =>
             {
                 DownloadData((pizzas) =>
                 {
+                    if( pizzas != null){
+                        listView.ItemsSource = GetPizzaCells(GetPizzasFromTri(tri, pizzas),pizzasFav);
+                    }
                     listView.IsRefreshing = false;
-                    listView.ItemsSource = GetPizzasFromTri(tri, pizzas);
+                    
                 });
 
             });
+
+
 
             //mettre ma fonction ici
             DownloadData((pizzas) =>
             {
                 listView.IsVisible = true;
                 waitLayout.IsVisible = false;
-                listView.ItemsSource = GetPizzasFromTri(tri, pizzas);
+                if (pizzas != null)
+                {
+                    listView.ItemsSource = GetPizzaCells(GetPizzasFromTri(tri, pizzas),pizzasFav);
+                }
             });
 
 
@@ -64,31 +108,37 @@ namespace PizzaApp
 
             using (var webClient = new WebClient())
             {
-                webClient.DownloadStringCompleted += (sender, e) =>
+                webClient.DownloadFileCompleted += (sender, e) =>
                 {
-                    try
+
+                    Exception ex = e.Error;
+
+                    if(ex == null)
                     {
-                        pizzas = JsonConvert.DeserializeObject<List<Pizza>>(e.Result);
-                    }
-                    catch (Exception ex)
-                    {
+                        File.Copy(tempFile, jsonFile, true);
+                        string pizzaJson = File.ReadAllText(jsonFile);
+                        pizzas = JsonConvert.DeserializeObject<List<Pizza>>(pizzaJson);
                         Device.BeginInvokeOnMainThread(() =>
                         {
-                            DisplayAlert("Erreur", "Une erreur réseau s'est produite: " + ex.Message, "OK");
+                            action.Invoke(pizzas);
+
+                        });
+
+                    }
+                    else
+                    {
+                        Device.BeginInvokeOnMainThread(async () =>
+                        {
+                            await DisplayAlert("Erreur", "Une erreur réseau s'est produite: " + ex.Message, "OK");
                             action.Invoke(null);
                         });
                     }
 
 
-                    Device.BeginInvokeOnMainThread(() =>
-                    {
-                        action.Invoke(pizzas);
-
-                    });
-
+                    
                 };
 
-                webClient.DownloadStringAsync(new Uri(URL));
+                webClient.DownloadFileAsync(new Uri(URL), tempFile);
             }
         }
 
@@ -105,11 +155,14 @@ namespace PizzaApp
                     break;
 
                 case e_tri.TRI_PRIX:
+                    tri = e_tri.TRI_FAV; ;
+                    break;
+                case e_tri.TRI_FAV:
                     tri = e_tri.TRI_AUCUN;
                     break;
             }
             sortImage.Source = GetImageSourceFromTri(tri);
-            listView.ItemsSource = GetPizzasFromTri(tri, pizzas);
+            listView.ItemsSource = GetPizzaCells(GetPizzasFromTri(tri, pizzas),pizzasFav);
 
             Application.Current.Properties[KEY_TRI] = (int)tri;
             Application.Current.SavePropertiesAsync();
@@ -133,6 +186,10 @@ namespace PizzaApp
                     res = "sort_prix";
                     break;
 
+                case e_tri.TRI_FAV:
+                    res = "sort_fav";
+                    break;
+
                 default:
                     res = "sort_none";
                     break;
@@ -147,6 +204,7 @@ namespace PizzaApp
                 return null;
             }
 
+            //copie de la liste de toutes les pizzas
             List<Pizza> copy = new List<Pizza>(l);
 
             switch (tri)
@@ -163,11 +221,71 @@ namespace PizzaApp
                     copy.Sort((emp1, emp2) => emp2.prix.CompareTo(emp1.prix));
                     break;
 
+                case e_tri.TRI_FAV:
+                    foreach(Pizza p in l)
+                    {
+                        if (!pizzasFav.Contains(p.nom))
+                        {
+                            copy.Remove(p);
+                        }
+                    }
+                    copy.Sort((emp1, emp2) => emp1.Titre.CompareTo(emp2.Titre));
+                    break;
+
                 default:
                     return l;
                     break;
             }
             return copy;
+        }
+
+        private List<PizzaCell> GetPizzaCells(List<Pizza> pizzas, List<String> f)
+        {
+            List<PizzaCell> res = new List<PizzaCell>();
+
+            if(pizzas == null)
+            {
+                return res;
+            }
+
+            foreach(Pizza p in pizzas)
+            {
+                bool isFav = f.Contains(p.nom);
+                res.Add(new PizzaCell { pizza = p, isFavorite= isFav, favChangedAction = OnFavPizzaChanged });
+            }
+
+            return res;
+
+        }
+
+        private void OnFavPizzaChanged(PizzaCell pizzaCell)
+        { 
+
+            bool isInFavList = pizzasFav.Contains(pizzaCell.pizza.nom);
+            if(isInFavList && !pizzaCell.isFavorite)
+            {
+                pizzasFav.Remove(pizzaCell.pizza.nom);
+            }else if(!isInFavList && pizzaCell.isFavorite)
+            {
+                pizzasFav.Add(pizzaCell.pizza.nom);
+            }
+            SaveFavList();
+        }
+
+        private void SaveFavList()
+        {
+            string json = JsonConvert.SerializeObject(pizzasFav);
+            Application.Current.Properties[KEY_FAV] = json;
+            Application.Current.SavePropertiesAsync();
+        }
+
+        private void LoadFavList()
+        {
+            if (Application.Current.Properties.ContainsKey(KEY_FAV))
+            {
+                string json = Application.Current.Properties[KEY_FAV].ToString();
+                pizzasFav = JsonConvert.DeserializeObject<List<string>>(json);
+            }
         }
     }
 }
---
+++ PizzaApp/PizzaApp/Model/PizzaCell.cs
@@ -0,0 +1,44 @@
+﻿using PizzaApp.Extensions;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PizzaApp.Model
+{
+    public class PizzaCell : INotifyPropertyChanged
+    {
+        public bool isFavorite { get; set; }
+
+        public Pizza pizza { get; set; }
+
+        public string imageSource
+        {
+            get { return isFavorite ? "star2.png" : "star1.png"; }
+        }
+
+        public Command FavClickCommand { get; set; }
+
+        public Action<PizzaCell> favChangedAction { get; set; }
+
+        public PizzaCell()
+        {
+            FavClickCommand = new Command((obj) =>
+            {
+                isFavorite = !isFavorite;
+                OnPropertyChanged("imageSource");
+                favChangedAction.Invoke(this);
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
